d6de398d52fc0db5b67ad54f2f4e21dc4043b7d8
Nick <nick.moi@gmail.com>
2019-09-23 02:08:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_hash = {}
+  
+  data.each do |color_gender_lives, value|
+    value.each do |key, all_names|
+      all_names.each do |name|
+        if new_pigeon_hash[name].nil?
+          new_pigeon_hash[name] = {}
+        end
+        
+        if new_pigeon_hash[name][color_gender_lives].nil?
+          new_pigeon_hash[name][color_gender_lives] = []
+        end
+        
+        new_pigeon_hash[name][color_gender_lives].push(key.to_s)
+      end
+    end
+  end
+  
+  new_pigeon_hash
 end
